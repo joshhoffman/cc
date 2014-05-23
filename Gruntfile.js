2cffbbcb43a4c0c0f59944a1403568ba67c62028
josh <makaze@gmail.com>
2014-05-23 15:36:37 +0000

    Add grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+    // load plugins
+    [
+        'grunt-contrib-coffee',
+        'grunt-coffeelint'
+    ].forEach(function(task) {
+            grunt.loadNpmTasks(task);
+        });
+
+    grunt.initConfig({
+        coffee: {
+            compile: {
+                files: {
+                    'Greed.js': 'Greed.coffee'
+                }
+            }
+        },
+        coffeelint: {
+            options: {
+                configFile: 'coffeelint.json'
+              },
+              app: [
+                  '*.coffee'
+              ]
+          },
+        watch: {
+            coffee: {
+                files: [
+                    '*.coffee'
+                ],
+                tasks: [
+                    'coffeelint',
+                    'coffee'
+                ]
+            }
+        }
+    });
+
+    //grunt.registerTask('default', ['cafemocha', 'jshint', 'less', 'notify:cafemocha'])
+    grunt.registerTask('default', ['coffee', 'coffeelint'])
+}
